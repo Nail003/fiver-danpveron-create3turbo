d7f2a1a73a74344d40ce337b3f40a28743f1b8bc
Nail003 <danialzafarstudy003@gmail.com>
2024-12-02 11:24:53 +0500

    feat: add vitest and necessary dependencies for nextjs workspace

---
+++ apps/nextjs/vitest.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    globals: true, // Enables global test APIs like `describe` and `it`
+    environment: "jsdom", // Use 'node' for backend projects
+    coverage: {
+      reporter: ["text", "json", "html"], // Optional: Generates coverage reports
+    },
+  },
+});
