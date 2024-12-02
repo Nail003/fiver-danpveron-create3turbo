6822919e01b250873601d50602aece4aa12939d1
Nail003 <danialzafarstudy003@gmail.com>
2024-12-02 14:20:52 +0500

    feat: add test cases for auth-showcase.tsx

--- apps/nextjs/src/app/_components/__test__/auth-showcase.test.tsx
+++ apps/nextjs/src/app/_components/__test__/auth-showcase.test.tsx
@@ -8,6 +8,29 @@ import { describe, expect, it, vi } from "vitest";
 
 import { AuthShowcase } from "../auth-showcase";
 
+// Mock components
+vi.mock("@acme/ui/button", () => {
+  type MockButtonType = {
+    children: React.ReactNode;
+    formAction: () => {};
+  };
+
+  return {
+    Button: ({ children, formAction }: MockButtonType) => {
+      return (
+        <button
+          onClick={async (e) => {
+            e.preventDefault();
+            await formAction();
+          }}
+        >
+          {children}
+        </button>
+      );
+    },
+  };
+});
+
 // Mock the `auth`, `signIn`, and `signOut` functions
 vi.mock("@acme/auth", () => ({
   auth: vi.fn(),
@@ -18,52 +41,58 @@ vi.mock("@acme/auth", () => ({
 const { auth, signIn, signOut } = await import("@acme/auth");
 
 describe("AuthShowcase", () => {
-  it("renders", async () => {
+  it("renders sign-in button when no session is active", async () => {
     auth.mockResolvedValue(null);
 
     render(await AuthShowcase());
 
     // Check if the sign-in button is displayed
-    const signInButton = screen.getByRole("button", {
-      name: /sign in with discord/i,
-    });
+    const signInButton = screen.getByRole("button");
     expect(signInButton).toBeInTheDocument();
   });
 
-  // it("renders sign-in button when no session is active", async () => {
-  //   auth.mockResolvedValue(null);
+  it("signIn the user when clicked on signIn button", async () => {
+    auth.mockResolvedValue(null);
 
-  //   render(await AuthShowcase());
+    render(await AuthShowcase());
 
-  //   // Check if the sign-in button is displayed
-  //   const signInButton = screen.getByRole("button", {
-  //     name: /sign in with discord/i,
-  //   });
-  //   expect(signInButton).toBeInTheDocument();
+    // Check if the sign-in button is displayed
+    const signInButton = screen.getByRole("button");
 
-  //   // Simulate clicking the sign-in button
-  //   await userEvent.click(signInButton);
-  //   expect(signIn).toHaveBeenCalledWith("discord");
-  // });
+    // Simulate clicking the sign-in button
+    await userEvent.click(signInButton);
+    expect(signIn).toHaveBeenCalledWith("discord");
+  });
 
-  // it("renders logged-in view when a session exists", async () => {
-  //   auth.mockResolvedValue({
-  //     user: { name: "John Doe" },
-  //   });
+  it("renders logged-in view when a session exists", async () => {
+    auth.mockResolvedValue({
+      user: { name: "John Doe" },
+    });
 
-  //   render(await AuthShowcase());
+    render(await AuthShowcase());
 
-  //   // Check if the logged-in view is displayed
-  //   expect(screen.getByText(/logged in as john doe/i)).toBeInTheDocument();
+    // Check if the logged-in view is displayed
+    expect(screen.getByText(/logged in as john doe/i)).toBeInTheDocument();
 
-  //   // Check if the sign-out button is displayed
-  //   const signOutButton = screen.getByRole("button", { name: /sign out/i });
-  //   expect(signOutButton).toBeInTheDocument();
+    // Check if the sign-out button is displayed
+    const signOutButton = screen.getByRole("button");
+    expect(signOutButton).toBeInTheDocument();
+  });
 
-  //   // Simulate clicking the sign-out button
-  //   await userEvent.click(signOutButton);
-  //   expect(signOut).toHaveBeenCalled();
-  // });
+  it("logsout the user when signOut button is pressed", async () => {
+    auth.mockResolvedValue({
+      user: { name: "John Doe" },
+    });
+
+    render(await AuthShowcase());
+
+    // Check if the sign-out button is displayed
+    const signOutButton = screen.getByRole("button");
+
+    // Simulate clicking the sign-out button
+    await userEvent.click(signOutButton);
+    expect(signOut).toHaveBeenCalled();
+  });
 
   // it("handles server-side errors gracefully", async () => {
   //   // Simulate an error in the `auth` function
