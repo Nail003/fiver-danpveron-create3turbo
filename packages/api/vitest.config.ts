b4b038041f28df4aa16a45a42964c8a98f45c516
Nail003 <danialzafarstudy003@gmail.com>
2024-12-03 11:40:48 +0500

    fix: updated comment

--- packages/api/vitest.config.ts
+++ packages/api/vitest.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     globals: true, // Enables global test APIs like `describe` and `it`
-    environment: "node", // Use 'node' for backend projects
+    environment: "node", // Use 'jsdom' for frontend projects
     coverage: {
       reporter: ["text", "json", "html"], // Optional: Generates coverage reports
     },
